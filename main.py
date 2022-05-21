1329119399ded6fcc3c8d2d43a9fabdcc7d67fd5
MDirt57 <maksdirt1@gmail.com>
2022-05-21 18:23:33 +0300

    Menu and timer changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from threading import*
@@ -13,7 +14,7 @@ class Menu(Screen):
 
     def __init__(self, **kwargs):
         super(Menu, self).__init__(**kwargs)
-        self.tasks = []
+        self.tasks = {}
         self.add_btn = Button(text = '+', font_size = 36, size_hint_y = None, height = self.height/2)
         self.add_btn.bind(on_press = self.add_task)
         self.edit_marker = 0
@@ -22,29 +23,38 @@ class Menu(Screen):
         if self.edit_marker == 0:
             self.ids.task_list.add_widget(self.add_btn)
             self.edit_marker = 1
-            for j in self.tasks:
-                j[1].text += ' delete'
-                j[1].bind(on_press = lambda j: self.delete_task(j))
+##            for j in self.tasks.values():
+##                j.width = self.width/2
+##                dell = Button(text = 'DEL', font_size = 36, size_hint_x = None, width = self.width/3)
+##                self.ids.task_list.add_widget(dell)
         else:
             self.ids.task_list.remove_widget(self.add_btn)
             self.edit_marker = 0
-            for j in self.tasks:
-                text = '---'
-                j[1].text = text
-                j[1].bind(on_press = lambda name: self.op(text))
+            for j in self.tasks.values():
+                j.bind(on_press = lambda j: self.get_start(j.text))
+
+
+    def create_task(self, inp, name):
+        self.tasks[inp].text = name
+        self.ids.task_list.remove_widget(inp)
 
     def add_task(self, instance):
         self.ids.task_list.remove_widget(self.add_btn)
-        task = Button(text = '---', font_size = 36, size_hint_y = None, height = self.height/10)
-        self.tasks.append((task.text, task))
+        task = Button(text = '', font_size = 36, size_hint_y = None, height = self.height/10)
+        name = TextInput(multiline = False, size_hint_y = None, height = self.height/18)
+        name.bind(on_text_validate = lambda name: self.create_task(name, name.text))
+        self.ids.task_list.add_widget(name)
+        self.tasks[name] = task
         self.ids.task_list.add_widget(task)
         self.ids.task_list.add_widget(self.add_btn)
 
     def delete_task(self, btn):
         self.ids.task_list.remove_widget(btn)
+        print(self.tasks)
 
-    def op(self, name):
-        print(name)
+    def get_start(self, name):
+        self.parent.get_screen('timer').ids.task_name.text = name
+        self.parent.current = 'timer'
 
 class History(Screen):
     pass
@@ -63,23 +73,33 @@ class Timer(Screen):
         print(sec)
 
     def start(self):
-        self.iscounting = True
-        self.t1.start()
+        if not self.iscounting:
+            self.ids.start_pause.text = 'Pause'
+            self.iscounting = True
+            self.t1.start()
         
     def pause(self):
         self.iscounting = False
         self.t1.join()
         self.t1.do_run = False
 
-    def detect_diff(self):
-        return round(time.perf_counter())
+    def zeros_format(self, sec):
+        if sec < 10:
+            return f"0{sec}"
+        return str(sec)
+
+    def time_format(self, sec):
+        if sec > 60 and sec < 3600:
+            return f"00:{self.zeros_format(sec // 60)}:{self.zeros_format(sec % 60)}"
+        elif sec > 3600:
+            return f"{self.zeros_format(sec // 3600)}:{self.zeros_format(sec % 3600 // 60)}:{self.zeros_format(sec % 60)}"
+        return f"00:00:{self.zeros_format(sec % 60)}"
 
     def seconds(self):
-        diff = self.detect_diff()
+        diff = round(time.perf_counter())
         while self.iscounting:
             self.mstopwatch = round(time.perf_counter()) - diff
-            sec = str(self.mstopwatch) if self.mstopwatch > 9 else f"0{self.mstopwatch}"
-            self.ids.stopwatch.text = "00:" + sec
+            self.ids.stopwatch.text = self.time_format(self.mstopwatch)
 
 
 class WindowManager(ScreenManager):
